b61c3efbc6dad08cfc16580ae47adbfd5f5657b3
Mmora0 <mamora171@misena.edu.co>
2023-11-27 18:08:45 -0500

    se agrega enemigo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Transform Derecha;
+    [SerializeField] Transform Izquierda;
+    Transform inicio;
+    Transform final;
+    Transform miTF;
+    float velocidad;
+    SpriteRenderer miSR;
     // Start is called before the first frame update
     void Start()
     {
-        
+        miTF = GetComponent<Transform>();
+        miTF.position = Izquierda.position;
+        inicio = Izquierda;
+        final = Derecha;
+        velocidad = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        miTF.Translate((final.position-inicio.position)*velocidad*Time.deltaTime);
+        if(Vector3.Distance(miTF.position, Derecha.position)< 0.1f)
+        {
+            inicio = Derecha;
+            final = Izquierda;
+            miSR.flipX = true;
+        }
+        else if(Vector3.Distance(miTF.position, Izquierda.position) < 0.1f)
+        {
+            inicio = Izquierda;
+            final = Derecha;
+            miSR.flipX = false;
+        }
     }
 }
